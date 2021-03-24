b85ba523bde2e616aec4a1c6c95e892126098263
Aryshia <77385254+Aryshia@users.noreply.github.com>
2021-03-24 15:08:23 +0100

    Update UIMorpion.java

--- src/morpion/UIMorpion.java
+++ src/morpion/UIMorpion.java
@@ -53,22 +53,17 @@ public class UIMorpion extends JDialog {
         });
 
         // call onCancel() on ESCAPE
-        contentPane.registerKeyboardAction(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        contentPane.registerKeyboardAction(e -> onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
     }
 
     private void onCancel() {
-        // add your code here if necessary
         dispose();
+
+
     }
 
     private void case1() {
-        button1.setBackground(Color.blue);
-        button1.setText("X");
-        button1.setForeground((Color.white));
+
     }
 
     private void case2() {
@@ -103,7 +98,7 @@ public class UIMorpion extends JDialog {
 
     }
 
-    public static void main(String[] args) {
+    public void ouvrir() {
         UIMorpion dialog = new UIMorpion();
         dialog.pack();
         dialog.setVisible(true);

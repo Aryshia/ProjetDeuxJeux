617c2034c5303dc1d16c0e9281d54ed53f70ee7b
Aryshia <77385254+Aryshia@users.noreply.github.com>
2021-03-24 14:28:37 +0100

    Update UIMorpion.java

--- src/morpion/UIMorpion.java
+++ src/morpion/UIMorpion.java
@@ -17,71 +17,32 @@ public class UIMorpion extends JDialog {
     private JButton button7;
     private JButton button8;
     private JButton button9;
+    // TODO: Faire une liste de boutons
 
     public UIMorpion() {
         setContentPane(contentPane);
         setModal(true);
-        getRootPane().setDefaultButton(buttonOK);
+        getRootPane().setDefaultButton(button1);
 
-        button1.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                case1();
-            }
-        });
+        button1.addActionListener(e -> case1());
 
-        button2.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                case2();
-            }
-        });
+        button2.addActionListener(e -> case2());
 
-        button3.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                case3();
-            }
-        });
+        button3.addActionListener(e -> case3());
 
-        button4.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                case4();
-            }
-        });
+        button4.addActionListener(e -> case4());
 
-        button5.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                case5();
-            }
-        });
+        button5.addActionListener(e -> case5());
 
-        button6.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                case6();
-            }
-        });
+        button6.addActionListener(e -> case6());
 
-        button7.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                case7();
-            }
-        });
+        button7.addActionListener(e -> case7());
 
-        button8.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                case8();
-            }
-        });
+        button8.addActionListener(e -> case8());
 
-        button9.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                case9();
-            }
-        });
+        button9.addActionListener(e -> case9());
 
-        buttonCancel.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        });
+        buttonCancel.addActionListener(e -> onCancel());
 
         // call onCancel() when cross is clicked
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
@@ -99,18 +60,15 @@ public class UIMorpion extends JDialog {
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
     }
 
-    private void onOK() {
-        // add your code here
-        dispose();
-    }
-
     private void onCancel() {
         // add your code here if necessary
         dispose();
     }
 
     private void case1() {
-
+        button1.setBackground(Color.blue);
+        button1.setText("X");
+        button1.setForeground((Color.white));
     }
 
     private void case2() {
